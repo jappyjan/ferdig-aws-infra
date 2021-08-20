2d23099be820e4131916bc904197e565efc314db
Jan Jaap <mail@janjaap.de>
2021-08-20 14:21:17 +0200

    ci: github workflow

--- lib/ferdig-stack.ts
+++ lib/ferdig-stack.ts
@@ -6,37 +6,36 @@ import {Cluster, ContainerImage} from 'aws-cdk-lib/aws-ecs';
 import {Vpc} from 'aws-cdk-lib/aws-ec2';
 
 export class FerdigStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
-    super(scope, id, props);
+    constructor(scope: Construct, id: string, props?: StackProps) {
+        super(scope, id, props);
 
-    new Bucket(this, 'ferdig-files', {
-      accessControl: BucketAccessControl.PRIVATE,
-      bucketName: 'ferdig-files',
-      publicReadAccess: false,
-    });
+        new Bucket(this, 'ferdig-files', {
+            accessControl: BucketAccessControl.PRIVATE,
+            bucketName: 'ferdig-files',
+            publicReadAccess: false,
+        });
 
-    const ferdigFargateVPC = new Vpc(this, 'ferdig-fargate-vpc', {
-      maxAzs: 2,
-    });
+        const ferdigFargateVPC = new Vpc(this, 'ferdig-fargate-vpc', {
+            maxAzs: 2,
+        });
 
-    const ferdigFargateCluster = new Cluster(this, 'ferdig-fargate-cluster', {
-      clusterName: 'ferdig-fargate-cluster',
-      vpc: ferdigFargateVPC
-    });
+        const ferdigFargateCluster = new Cluster(this, 'ferdig-fargate-cluster', {
+            clusterName: 'ferdig-fargate-cluster',
+            vpc: ferdigFargateVPC,
+        });
 
-    const ferdigImage = ContainerImage.fromRegistry('ferdig/ferdig');
+        const ferdigImage = ContainerImage.fromRegistry('ferdig/ferdig');
 
-    new ApplicationLoadBalancedFargateService(this, 'ferdig-fargate-service', {
-      cluster: ferdigFargateCluster,
-      cpu: 256,
-      memoryLimitMiB: 1024,
-      desiredCount: 1,
-      publicLoadBalancer: true,
-      taskImageOptions: {
-        image: ferdigImage
-      }
-    })
-
-    // The code that defines your stack goes here
-  }
+        new ApplicationLoadBalancedFargateService(this, 'ferdig-fargate-service', {
+            cluster: ferdigFargateCluster,
+            cpu: 256,
+            memoryLimitMiB: 1024,
+            desiredCount: 1,
+            publicLoadBalancer: true,
+            taskImageOptions: {
+                image: ferdigImage,
+            },
+            domainName: 'app.ferdig.de'
+        });
+    }
 }
